75b63c3e1b8d6a6b7018f8996e344fbb68edb1fb
Nikita <81490302+maketryuk@users.noreply.github.com>
2021-08-16 11:23:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,11 +4,24 @@ window.onload = function () {
   var preloader = document.getElementById('preloader');
   preloader.style.display = 'none';
 };
-"use strict";
-"use strict";
-"use strict";
-"use strict";
-"use strict";
+
+$(window).bind('scroll', function () {
+  var elementHeight = $('.about__pretitle').height();
+  var containerHeight = $('.about-list').height();
+  var distanceToScroll = containerHeight - elementHeight;
+
+  if (distanceToScroll > 0) {
+    if ($(window).scrollTop() < distanceToScroll) {
+      $('.about__pretitle').addClass('pos-fixed');
+      $('.about__pretitle').removeClass('stop');
+    } else if ($(window).scrollTop() >= distanceToScroll) {
+      $('.about__pretitle').addClass('stop');
+      $('.about__pretitle').removeClass('pos-fixed');
+    } else {
+      $('.about__pretitle').removeClass('stop');
+    }
+  }
+});
 
 if (window.matchMedia("(min-width: 992px)").matches) {
   $(function () {
@@ -28,42 +41,6 @@ if (window.matchMedia("(min-width: 992px)").matches) {
     });
   });
 }
-"use strict";
-
-var photographerMain = new Swiper('.photographer-slider', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  autoplay: true,
-  loop: true,
-  loopedSlides: 50,
-  speed: 1500,
-  pagination: {
-    el: '.photographer-pagination'
-  },
-  navigation: {
-    nextEl: '.photographer-slider-next',
-    prevEl: '.photographer-slider-prev'
-  }
-});
-var photographerThumbs = new Swiper('.photographer-thumbs', {
-  slidesPerView: 5,
-  slideToClickedSlide: true,
-  loopedSlides: 50,
-  loop: true,
-  speed: 1500,
-  spaceBetween: 30,
-  breakpoints: {
-    1351: {
-      spaceBetween: 56
-    },
-    1151: {
-      spaceBetween: 30
-    }
-  }
-});
-photographerMain.controller.control = photographerThumbs;
-photographerThumbs.controller.control = photographerMain;
-"use strict";
 
 var galleryMain = new Swiper('.hero-slider', {
   slidesPerView: 1,
@@ -104,8 +81,40 @@ var galleryThumbs = new Swiper('.hero-thumbs', {
   }
 }); // galleryMain.controller.control = galleryThumbs;
 // galleryThumbs.controller.control = galleryMain;
-"use strict";
-"use strict";
+
+var photographerMain = new Swiper('.photographer-slider', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: true,
+  loop: true,
+  loopedSlides: 50,
+  speed: 1500,
+  pagination: {
+    el: '.photographer-pagination'
+  },
+  navigation: {
+    nextEl: '.photographer-slider-next',
+    prevEl: '.photographer-slider-prev'
+  }
+});
+var photographerThumbs = new Swiper('.photographer-thumbs', {
+  slidesPerView: 5,
+  slideToClickedSlide: true,
+  loopedSlides: 50,
+  loop: true,
+  speed: 1500,
+  spaceBetween: 30,
+  breakpoints: {
+    1351: {
+      spaceBetween: 56
+    },
+    1151: {
+      spaceBetween: 30
+    }
+  }
+});
+photographerMain.controller.control = photographerThumbs;
+photographerThumbs.controller.control = photographerMain;
 
 var studioMain = new Swiper('.studio-slider', {
   slidesPerView: 1,
@@ -146,8 +155,6 @@ var studioThumbs = new Swiper('.studio-thumbs', {
 });
 studioMain.controller.control = studioThumbs;
 studioThumbs.controller.control = studioMain;
-"use strict";
-"use strict";
 
 $(function () {
   $('.burger').click(function () {
